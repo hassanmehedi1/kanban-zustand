ede3769fa8d61d7a6b1e44ba073d32569e995aeb
Mehedi <rehan51322@gmail.com>
2023-11-13 14:21:50 +0600

    drag and drop added

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -8,16 +8,34 @@ import { shallow } from "zustand/shallow";
 export default function Column({ state }) {
   const [text, setText] = useState("");
   const [open, setOpen] = useState(false);
-  // const [drop, setDrop] = useState(false);
+  const [drop, setDrop] = useState(false);
 
   const tasks = useStore(
     (store) => store.tasks.filter((task) => task.state === state),
     shallow
   );
   const addTask = useStore((store) => store.addTask);
+  const setDraggedTask = useStore((store) => store.setDraggedTask);
+  const draggedTask = useStore((store) => store.draggedTask);
+  const moveTask = useStore((store) => store.moveTask);
 
   return (
-    <div>
+    <div
+      className={classNames("column", { drop: drop })}
+      onDragOver={(e) => {
+        setDrop(true);
+        e.preventDefault();
+      }}
+      onDragLeave={(e) => {
+        setDrop(false);
+        e.preventDefault();
+      }}
+      onDrop={(e) => {
+        setDrop(false);
+        moveTask(draggedTask, state);
+        setDraggedTask(null);
+      }}
+    >
       <div className="titleWrapper">
         <p>{state}</p>
         <button onClick={() => setOpen(true)}>Add</button>
